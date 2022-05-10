9d25ff594bd59d1ad9631f27949d4e1d0191278c
Joseph Macam <jdmacam@gmail.com>
2022-05-09 19:58:38 -0700

    finished projects and work history scenes

--- main.js
+++ main.js
@@ -2,14 +2,18 @@
 // Libraries
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-import { BoxGeometry } from 'https://unpkg.com/three/src/geometries/BoxGeometry.js';
+// import { BoxGeometry } from 'https://unpkg.com/three/src/geometries/BoxGeometry.js';
+// import { MeshBasicMaterial } from 'three';
+// import { CylinderGeometry } from 'three';
 
 // Loaders
 const textureLoader = new THREE.TextureLoader();
+const gtlfLoader = new GLTFLoader();
 
 function main() {
 	const canvas = document.querySelector('#c');
-	const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+	const renderer = new THREE.WebGLRenderer({ canvas, alpha: true});
+	renderer.shadowMap.enabled = true;
 	// do not clear output before rendering frame
 	renderer.autoClear = false;
 	const width = canvas.clientWidth;
@@ -55,34 +59,78 @@ function main() {
 		]
 	}
 
-	// // Portrait model scene
-	// function setupSceneJoe() {
-	// 	const texture = textureLoader.load('images/joe.jpg')
-	// 	const sceneInfo = makeScene(document.querySelector('#joe-portrait'));
-	// 	const geometry = new THREE.BoxGeometry(2, 2, 2);
-	// 	const material = new THREE.MeshPhongMaterial({ map: texture });
-	// 	const mesh = new THREE.Mesh(geometry, material);
-	// 	sceneInfo.scene.add(mesh);
-	// 	sceneInfo.mesh = mesh;
-	// 	return sceneInfo;
-	// }
 
 	// Project models scene
 	function setupSceneProjects() {
 		const sceneInfo = makeScene(document.querySelector('#project-models'));
-		const geometry = new THREE.BoxGeometry(1, 1, 0.125);
-		const material = new THREE.MeshPhongMaterial({ color: 'red' });
-		const mesh = new THREE.Mesh(geometry, material);
-		mesh.position.set(0,1.3,0)
-		sceneInfo.scene.add(mesh);
-		sceneInfo.mesh = mesh;
+		
+		// Keyboard Object
+		const keeb_ratio = 1.2
+		const geometryKeeb = new THREE.BoxGeometry(
+			1 * keeb_ratio, 
+			.39 * keeb_ratio, 
+			0.15 * keeb_ratio
+		);
+		const materialKeeb = frontBoxMaterial('images/laser.png');
+		const meshKeeb = new THREE.Mesh(geometryKeeb, materialKeeb);
+		meshKeeb.position.set(0,1.1,0)
+		meshKeeb.rotation.x = .25;
+		sceneInfo.scene.add(meshKeeb);
 
-		const geometry2 = new THREE.BoxGeometry(1, 1, 0.125);
-		const material2 = new THREE.MeshPhongMaterial({ color: 'red' });
-		const mesh2 = new THREE.Mesh(geometry2, material2);
-		mesh2.position.set(0,-0.2,0)
-		sceneInfo.scene.add(mesh2);
-		sceneInfo.mesh = mesh2;
+
+		// Gundam Object
+		var gundam;
+		gtlfLoader.load('models/strike_gundam_posed.glb', function (gltf) {
+			const model = gltf.scene;
+			model.traverse(function (node) {
+				if (node.isMesh) { node.castShadow = true; }
+			});
+			model.scale.set(.5,.5,.5);
+			model.position.set(0,-.65,0);
+			const gundamPosition = model.position;
+			// console.log(model.position);
+			sceneInfo.scene.add(model);
+
+			//Gundam Base
+			const geometryBase = new THREE.CylinderGeometry(.5,.5,.2,32);
+			const materialBase = new THREE.MeshBasicMaterial( { map: textureLoader.load('images/carbonfiber.jpg') });
+			const meshBase = new THREE.Mesh(geometryBase, materialBase);
+			meshBase.position.set(0, gundamPosition.y - .11, 0);
+			meshBase.receiveShadow = true;
+			sceneInfo.scene.add(meshBase)
+
+			//Gundam Lights
+			const light1 = new THREE.DirectionalLight(0xffffff, 5);
+			light1.position.set(
+				gundamPosition.x - 0.5,
+				gundamPosition.y + 0.5,
+				gundamPosition.z - 0.1
+			)
+			light1.castShadow = true;
+			light1.target = model;
+
+			const light2 = new THREE.DirectionalLight(0xffffff, 5);
+			light2.position.set(
+				gundamPosition.x + 0.5,
+				gundamPosition.y + 0.5,
+				gundamPosition.z - 0.1
+			)
+			light2.castShadow = true;
+			light2.target = model;
+			
+			const light3 = new THREE.DirectionalLight(0xffffff, 5);
+			light3.position.set(
+				gundamPosition.x,
+				gundamPosition.y - 0.5,
+				gundamPosition.z - 0.1
+			)
+			light3.castShadow = true;
+			light3.target = model;
+
+			sceneInfo.scene.add(light1)
+			sceneInfo.scene.add(light2);
+			sceneInfo.scene.add(light3);
+		});
 
 		return sceneInfo;
 	}
@@ -90,17 +138,17 @@ function main() {
 	// Work History models scene
 	function setupSceneWorkHistory() {
 		const sceneInfo = makeScene(document.querySelector('#work-history-models'));
-		const geometry = new THREE.BoxGeometry(1, 1, 0.125);
-		const material = new THREE.MeshPhongMaterial({ color: 'red' });
+		const geometry = new THREE.BoxGeometry(1, 1, .15);
+		const material = frontBoxMaterial('images/pnnl.png');
 		const mesh = new THREE.Mesh(geometry, material);
-		mesh.position.set(0,1.3,0)
+		mesh.position.set(0,1.2,0)
 		sceneInfo.scene.add(mesh);
 		sceneInfo.mesh = mesh;
 
 		const geometry2 = new THREE.BoxGeometry(1, 1, 0.125);
-		const material2 = new THREE.MeshPhongMaterial({ color: 'red' });
+		const material2 = frontBoxMaterial('images/chicostate.jpg');
 		const mesh2 = new THREE.Mesh(geometry2, material2);
-		mesh2.position.set(0,-.8,0)
+		mesh2.position.set(0,-.7,0)
 		mesh2.rotation.x = -0.1;
 		sceneInfo.scene.add(mesh2);
 		sceneInfo.mesh = mesh2;
@@ -108,27 +156,15 @@ function main() {
 		return sceneInfo;
 	}
 
-	// Education models scene
-	// function setupSceneEducation() {
-	// 	const sceneInfo = makeScene(document.querySelector('#education-models'));
-	// 	const geometry = new THREE.BoxGeometry(3, 3, 0.125);
-	// 	const material = new THREE.MeshPhongMaterial({ color: 'red' });
-	// 	const mesh = new THREE.Mesh(geometry, material);
-	// 	sceneInfo.scene.add(mesh);
-	// 	sceneInfo.mesh = mesh;
-	// 	return sceneInfo;
-	// }
 
-	// const sceneInfoJoe = setupSceneJoe();
 	const sceneInfoProjects = setupSceneProjects();
 	const sceneInfoWorkHistory = setupSceneWorkHistory();
-	// const sceneInfoEducation = setupSceneEducation();
 
 	// *************************************
 	// ******** Rendering/Animation ********
 	// *************************************
 	function hover_animation(object) {
-		object.rotation.y = Math.sin(Date.now() * 0.0005) * Math.PI * 0.02;
+		object.rotation.y = Math.sin(Date.now() * 0.0005) * Math.PI * 0.04;
 		object.position.y += Math.sin(Date.now() * 0.001) * Math.PI * 0.0001;
 	}
 
@@ -155,22 +191,23 @@ function main() {
 
 	function render() {
 		rendererToDisplaySize(renderer);
-		//Render scenes
-		// renderSceneInfo(sceneInfoJoe);
-		// hover_animation(sceneInfoJoe.mesh);
 
-		renderSceneInfo(sceneInfoProjects);
+		// ***** Render Scenes *****
+
+		// Projects Rendering
 		// console.log(sceneInfoProjects.scene.children)
-		hover_animation(sceneInfoProjects.scene.children[1]);
-		hover_animation(sceneInfoProjects.scene.children[2]);
+		renderSceneInfo(sceneInfoProjects);
+		const keebObj = sceneInfoProjects.scene.children[1];
+		const gundamObj = sceneInfoProjects.scene.children[2];
+		const gundamBaseObj = sceneInfoProjects.scene.children[3];
+		if(keebObj){hover_animation(keebObj);}
+		if(gundamObj){hover_animation(gundamObj);}
+		if(gundamBaseObj){hover_animation(gundamBaseObj);}
 
 		renderSceneInfo(sceneInfoWorkHistory);
 		// console.log(sceneInfoWorkHistory.scene.children)
 		hover_animation(sceneInfoWorkHistory.scene.children[1]);
 		hover_animation(sceneInfoWorkHistory.scene.children[2]);
-
-		// renderSceneInfo(sceneInfoEducation);
-		// hover_animation(sceneInfoEducation.mesh);
 
 		requestAnimationFrame(render);
 	}
